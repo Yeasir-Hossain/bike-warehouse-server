fb5bc5399b0e16942de1831fd3d945ee4ea63621
Yeasir-Hossain <yeasir06@gmail.com>
2022-05-07 04:03:40 +0600

    all items get

--- index.js
+++ index.js
@@ -9,20 +9,19 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
-const uri = `mongodb+srv://${process.env.DB_YEASIR}:${process.env.DB_PASS}@cluster0.nklab.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.25a2a.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 async function run(){
     try{
         await client.connect();
         const bikeCOllection = client.db('bikeHub').collection('bike');
-        const orderCollection = client.db('geniusCar').collection('order');
         //GET ALL BIKES
         app.get('/bike', async (req, res) => {
             const query = {}
             const cursor = serviceCOllection.find(query);
-            const services = await cursor.toArray();
-            res.send(services);
+            const bikes = await cursor.toArray();
+            res.send(bikes);
           })
     }
     finally{
